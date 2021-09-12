23d6256009db18806d094d1afafcc93a42f2e2fe
Tasnimul Alam <32575769+TOHIRalam@users.noreply.github.com>
2021-09-12 21:35:53 +0000

    Interface defined

--- index.ts
+++ index.ts
@@ -1,20 +1,10 @@
-// Normal function
-let logs = function(message : string) {
-  console.log(message);
+interface Point {
+  x : number,
+  y : number
 }
 
-let sum = function(a : number, b : number) {
-  return a + b;
-}
-console.log(sum(1, 2));
-
-let doSomething = function() {
-  return 10;
+let drawPoint = (point : Point) => {
+  return point.x + point.y;
 }
 
-// Arrow function
-
-let logs2 = (message : string) => console.log(message)
-let sum2 = (a : number, b : number) => a + b;
-console.log(sum2(1, 2));
-let doSomething2 = () => 10;
+console.log(drawPoint({x : 1, y : 2}));
