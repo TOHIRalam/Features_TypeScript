3c3e2814d0c583ac7a389b2debf7b595e59fbb2f
Tasnimul Alam <32575769+TOHIRalam@users.noreply.github.com>
2021-09-12 22:50:54 +0000

    error fixed

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 class Point {
-  x: number;
-  y: number;
+  x: number;      
+  y: number;      
   
   // Here, in the constructor x and y are optional
   constructor(x?: number, y?: number) {
-    this.x = x;
-    this.y = y;
+    this.x = x ? x : 0; // If x is null then value should be zero
+    this.y = y ? y : 0; // If y is null then value should be zero
   }
 
   draw = () => console.log(`X: ${this.x}, Y: ${this.y}`);
