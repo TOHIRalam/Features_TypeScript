bd709d74b69be8ebd7e6747edce27c0e3e8d1102
MrTOHIR <>
2021-09-12 12:04:04 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+let a : number;
+let b : string;
+let c : boolean;
+let d : any;
+let e : number[];
+let f : number[] = [1,2,3,4];
+let g : any[] = [1, true, "Hello World", 5.66];
+
+const colorRed = 0;
+const colorGreen = 1;
+const colorBlue = 2;
+
+enum Color { Red, Green, Blue }
