e7f9438a59b5ff68961d64a7c4e5f4f016aa3942
Tasnimul Alam <32575769+TOHIRalam@users.noreply.github.com>
2021-09-12 12:09:47 +0000

    datatypes & enum

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// Datatypes
 let a : number;
 let b : string;
 let c : boolean;
@@ -6,8 +7,16 @@ let e : number[];
 let f : number[] = [1,2,3,4];
 let g : any[] = [1, true, "Hello World", 5.66];
 
+// Constants
 const colorRed = 0;
 const colorGreen = 1;
 const colorBlue = 2;
 
+// enum
 enum Color { Red, Green, Blue }
+let backGroundColor = Color.Red;
+console.log(backGroundColor);
+
+enum Color2 { Red = 2, Green, Blue, Purple }
+let foreGroundColor = Color2.Blue;
+console.log(foreGroundColor);
