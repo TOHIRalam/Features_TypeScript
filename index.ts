087a9c9329b3f0d673dff1b15335ed986f603b4d
Tasnimul Alam <32575769+TOHIRalam@users.noreply.github.com>
2021-09-12 14:11:29 +0000

    normal function vs arrow function

--- index.ts
+++ index.ts
@@ -1,47 +1,20 @@
-/*
-  By default the data type of message variable is string.
-*/
-let message = "Hello World";
-let endsWith_World = message.endsWith("World");
-let endsWith_d = message.endsWith('d');
-let endsWith_Hello = message.endsWith("Hello");
+// Normal function
+let logs = function(message : string) {
+  console.log(message);
+}
 
-console.log(`message: ${message}`)
-console.log(`message ends with 'World': ${endsWith_World}`);   // true 
-console.log(`message ends with 'World': ${endsWith_d}`);       // true
-console.log(`message ends with 'World': ${endsWith_Hello}\n`);   // false
+let sum = function(a : number, b : number) {
+  return a + b;
+}
+console.log(sum(1, 2));
 
-/*
-  Here, the data type of message2 variable is set to any.
-  So, we won't get any intellisense suggestion for string function.
-  In this case we have to explicitly tell the typescript complier that 
-  this message2 variable is actually a string. This is called type assertion.
-  There are two ways to do type assertions.
-*/
+let doSomething = function() {
+  return 10;
+}
 
-let message2;
-message2 = "Hello World";
-let endsWith_World2 = message2.endsWith("World");
-let endsWith_d2 = message2.endsWith('d');
-let endsWith_Hello2 = message2.endsWith("Hello");
+// Arrow function
 
-console.log(`message2: ${message2}`)
-console.log(`message2 ends with 'World': ${endsWith_World2}`);   // true 
-console.log(`message2 ends with 'World': ${endsWith_d2}`);       // true
-console.log(`message2 ends with 'World': ${endsWith_Hello2}\n`);   // false
-
-
-/* Type assertions */
-
-let message3;
-message3 = "Hello World";
-let endsWith_World3 = (<string>message3).endsWith("World");
-let endsWith_d3 = (message3 as string).endsWith('d');
-let endsWith_Hello3 = message3.endsWith("Hello");
-
-console.log(`message3: ${message3}`)
-console.log(`message3 ends with 'World': ${endsWith_World2}`);   // true 
-console.log(`message3 ends with 'World': ${endsWith_d2}`);       // true
-console.log(`message3 ends with 'World': ${endsWith_Hello2}\n`);   // false
-
-
+let logs2 = (message : string) => console.log(message)
+let sum2 = (a : number, b : number) => a + b;
+console.log(sum2(1, 2));
+let doSomething2 = () => 10;
