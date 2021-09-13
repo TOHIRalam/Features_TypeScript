a4695fba799ee0397458949ffa6cc084d8801041
Tasnimul Alam <32575769+TOHIRalam@users.noreply.github.com>
2021-09-13 08:44:57 +0000

    get and set property

--- index.ts
+++ index.ts
@@ -1,36 +1,59 @@
 class Point {
-  x: number;      // By default public
-  y: number;      // By default public 
-  
-  // Here, in the constructor x and y are optional
-  constructor(x?: number, y?: number) {
-    this.x = x ? x : 0; // If x is null then value should be zero
-    this.y = y ? y : 0; // If y is null then value should be zero
-  }
-
-  draw = () => console.log(`X: ${this.x}, Y: ${this.y}`);
-
-  getDistance(another : Point) {
-    // ...
-  }
-}
-
-class Point2 {
-  /* Typescript compiler generates a field with exact same name and initialize it
-     Because we have used private access modifier. */ 
   constructor(private x?: number, private y?: number) { }
 
   draw = () => console.log(`X: ${this.x}, Y: ${this.y}`);
+  
+  // Getter and Setter function
+  getX = () => this.x;
+  getY = () => this.y;
 
-  getDistance(another : Point) {
-    // ...
+  setX(value : number) {
+    if (value < 0) {
+      throw new Error("Value cannot be less than zero!");
+    } else {
+      this.x = value;
+    }
+  }
+
+  setY = (value : number) => this.y = value < 0 ? 1 : value;
+}
+
+class Point2 {
+  constructor(private x: number, private y: number) { }
+
+  draw = () => console.log(`X: ${this.x}, Y: ${this.y}`);
+  
+  // get and set properties
+  get X() {
+    return this.x;
+  }
+  get Y() {
+    return this.y;
+  }
+
+  set X(value : number) {
+    if (value < 0) {
+      throw new Error("Value cannot be less than zero!");
+    } else {
+      this.x = value;
+    }
+  }
+
+  set Y (value : number) {
+    this.y = value < 0 ? 1 : value;
   }
 }
 
-let point1 = new Point(5, 6);
-point1.x = 7; point1.y = 9; // Because x and y are public 
+let point1 = new Point();
 
-let point2: Point2 = new Point2(10, 11);
+point1.setX(4);
+point1.setY(-3);
+
+
+let point2 = new Point2(0, 0);
+
+point2.X = 4;
+point2.Y = -3;
 
 point1.draw();
 point2.draw();
